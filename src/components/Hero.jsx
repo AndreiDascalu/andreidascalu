6714b2e7dfd60e90ada249f95a7b9e304da7750f
AndreiDascalu <dascaluandrei172001@gmail.com>
2023-03-19 22:04:39 +0100

    Add laptop 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,21 @@ const Hero = () => {
           <div className='flex flex-col justify-center
           items-center mt-5'>
             <div className='w-5 h-5 rounded-full bg-[#915eff]'/> 
-            
+            <div className='w-1 sm: h-80 h-40 violet-gradient' />  
           </div>
 
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi there, I'm <span className='text-[#915eff]'>Andrei</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-1 text-white-100`}>
+              Computer Science and Engineering undergraduate at TU Delft,
+              <br className='sm:block hidden'/> dedicated to exploring innovative technologies
+            </p>
+          </div>
+          
         </div>
+        <ComputersCanvas />
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
 
 const Computers = () => {
+  const [rotation, setRotation] = useState([-0.15, 0, -0.3])
+  const computer = useGLTF('./gaming_laptop/scene.gltf')
+
+  // Rotate the laptop on each frame
+  const onFrame = (e) => {
+    setRotation([rotation[0], rotation[1] + 0.001, rotation[2]])
+  }
+
   return (
-    <div>Computers</div>
+    <mesh onBeforeRender={onFrame}>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={0.7}/>
+      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={0.6} />
+      <primitive object={computer.scene} scale={1} position={[0, -2, 0]} rotation={rotation} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{position: [20, 3 ,10], fov:25}}
+      gl={{preserveDrawingBuffer:true}}
+    >
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls
+          enableZoom = {false}
+          maxPolarAngle={Math.PI/2}
+          minPolarAngle={Math.PI/2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
