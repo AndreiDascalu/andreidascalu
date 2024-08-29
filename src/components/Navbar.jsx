eee77e3208fe38f7a451818c378057c9836d90bb
AndreiDascalu <dascaluandrei172001@gmail.com>
2024-08-29 18:52:25 +0300

    Add CV

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,100 +1,107 @@
-import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import {logo, menu, close, white_logo, black_logo} from "../assets"
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close, white_logo, black_logo } from "../assets";
+import cvFile from "../assets/Andrei Dascalu - CV.pdf";
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
-  const [toggle, setToggle] = useState(false)
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={
-      `${styles.paddingX} w-full flex items-center py-3 fixed 
-      top-0 z-20 bg-primary`
-    }
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-3 fixed 
+      top-0 z-20 bg-primary`}
     >
-
-      <div className='w-full flex justify-between items-center 
-                      max-w-7xl mx-auto'>
-        <Link to="/"
-              className='flex items-center gap-2'  
-              onClick={() => {setActive("");
-                              window.scrollTo(0, 0);
-            }}  
+      <div
+        className="w-full flex justify-between items-center 
+                      max-w-7xl mx-auto"
+      >
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-          <img src ={white_logo} alt="white_logo" className='w-12 h-12 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+          <img
+            src={white_logo}
+            alt="white_logo"
+            className="w-12 h-12 object-contain"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Andrei Dascalu &nbsp;
-            <span className='sm:block hidden'>| Online Portfolio</span>
+            <span className="sm:block hidden">| Online Portfolio</span>
           </p>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {
-            navLinks.map((link) => (
-              <li
-                key={link.id}
-                className={`${active ===link.title
-                  ? "text-white"
-                  : "text-secondary"
-                }
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }
                 hover:text-white text-[18px]
                 font-medium cursor-pointer`}
-                onClick={() => setActive(link.title)}
-              >
-                <a href = {`#${link.id}`}>
-                  {link.title}
-                </a>
-              </li>
-            ))
-          }
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+          <li
+            className={`${
+              active === "CV" ? "text-white" : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+          >
+            <a href={cvFile} download>
+              CV
+            </a>
+          </li>
         </ul>
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className='w-[28px] h-[28px]
-          object-contain cursor-pointer'
-          onClick={() => setToggle(!toggle)}
-           />
-          <div className={`${!toggle ? 'hidden' : 'flex'} 
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px]
+          object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${!toggle ? "hidden" : "flex"} 
                           p-6 black-gradient absolute top-20
                           right-0 mx-4 my-2 min-w-[140px]
                           z-10 rounded-xl
-                        `}>
-            <ul className='list-none flex justify-end items-start
-            flex-col gap-4'>
-              {
-                navLinks.map((link) => (
-                  <li
-                    key={link.id}
-                    className={`${active ===link.title
-                      ? "text-white"
-                      : "text-secondary"
-                    }
+                        `}
+          >
+            <ul
+              className="list-none flex justify-end items-start
+            flex-col gap-4"
+            >
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }
                     font-poppins font-medium
                     cursor-pointer text-[16px]`}
-                    onClick={() => {
-                      setToggle(!toggle);
-                      setActive(link.title);
-                    }}
-                  >
-                    <a href = {`#${link.id}`}>
-                      {link.title}
-                    </a>
-                  </li>
-                ))
-              }
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
             </ul>
-
           </div>
-
         </div>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
